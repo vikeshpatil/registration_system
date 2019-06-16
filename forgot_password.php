359ce406ab1a425ce9d28ac2ab7ae059f5f4f0eb
Vikesh Patil <rajp8340@gmail.com>
2019-06-16 23:39:10 +0530

    added password recovery feature

--- forgot_password.php
+++ forgot_password.php
@@ -3,16 +3,63 @@
     <div class="container">
         <div class="content">
             <h2 class="heading">Password Recovery</h2>
-            <div class='notification'>You need to wait at lest 20 minutes for another request</div>
+            <?php
+
+              if(isset($_POST['password_recovery'])){
+                    $username = escape($_POST['user_name']);
+                    $email = escape($_POST['user_email']);
+
+                    $query = "SELECT * FROM user_details WHERE username = '$username' AND email = '$email'";
+                    $query_run = mysqli_query($connection, $query);
+
+                    if(!$query_run){
+                      die("Connection to database failed".mysqli_error($connection));
+                    }
+                    $row_num = mysqli_num_rows($query_run);
+                    if($row_num==1){
+                      if(!isset($_COOKIE['password_recovery'])){
+
+                      // password_recovery email
+                      date_default_timezone_set('Asia/Kolkata');
+                      $token = getToken(32);
+                      $encode_token = base64_encode(urlencode($token));
+                      $encrypted_email = base64_encode(urlencode($_POST['user_email']));
+                      $expire_date = date("Y-m-d H:i:s", time() + 60 * 5);
+                      $expire_date = base64_encode(urlencode($expire_date));
+
+                      $query = "UPDATE user_details SET validation_key ='$token' WHERE username='$username' AND email='$email' AND is_active=1";
+                      $query_run = mysqli_query($connection, $query);
+                      if(!$query_run){
+                        die("Unable to connect to database".mysqli_error());
+                      }else{
+                        $mail->Subject="Password recovery request";   //recipient
+                        $mail->addAddress($email);
+                        $mail->Body="Follow the link to reset password.<br>
+                                      <a href='localhost/projects/registration/new_password.php?eid={$encrypted_email}&token={$encode_token}&exd={$expire_date}'>click here</a> to reset password.
+                                      <br> This link is valid for only 5 minutes.";
+                        if($mail->send()){
+                          setcookie('password_recovery', getToken(16), time() + 60 * 5, '', '', '', true);
+                          echo "<div class='notification'>Check your email for password reset link.</div>";
+                        }
+                      }
+                    }else{
+                        echo "<div class='notification'>You must wait for at least 5 minutes for next link.</div>";
+                  }
+                }else echo "<div class='notification'>Sorry! user not found.</div>";
+              }
+
+             ?>
+
+
             <form action="" method="POST">
                 <div class="input-box">
-                    <input type="text" class="input-control" placeholder="Username" name="user-name" required>
+                    <input type="text" class="input-control" placeholder="Username" name="user_name" required>
                 </div>
                 <div class="input-box">
-                    <input type="email" class="input-control" placeholder="Email address" name="user-email" required>
+                    <input type="email" class="input-control" placeholder="Email address" name="user_email" required>
                 </div>
                 <div class="input-box">
-                    <input type="submit" class="input-submit" value="RECOVER PASSWORD" name="password-recovery" required>
+                    <input type="submit" class="input-submit" value="RECOVER PASSWORD" name="password_recovery" required>
                 </div>
             </form>
         </div>
--- new_password.php
+++ new_password.php
@@ -1,15 +1,81 @@
+<?php require_once 'include/db.php'; ?>
+<?php require_once 'include/functions.php'; ?>
 <?php  $current_page="new_password"; ?>
 <?php include_once('include/header.php');?>
     <div class="container">
         <div class="content">
             <h2 class="heading">New Password</h2>
-            <div class='notification'>Password updated successfully. <a href='login.php'>login now</a></div>
+
+              <?php
+                  if(isset($_GET['eid']) AND isset($_GET['token']) AND isset($_GET['exd'])){
+                    $email = urldecode(base64_decode($_GET['eid']));  //decrypting email from confirmation link
+                    $expire_date = urldecode(base64_decode($_GET['exd']));
+                    $validation_key = urldecode(base64_decode($_GET['token']));
+
+                    date_default_timezone_set('Asia/Kolkata');
+                    $current_date = date("Y-m-d H:i:s");
+
+                    if($current_date >= $expire_date){
+                      echo "<div class='notification'>Link expired</div>";
+                      $check = true;  //used to disable input fields if link is expired
+                    }else {
+
+                      if(isset($_POST['submit'])){
+                        $password = escape($_POST['new-password']);
+                        $confirm_password = escape($_POST['confirm-new-password']);
+
+                        //password validation
+                        if ($password==$confirm_password) {
+                            $pattern_pass = "/^.*(?=.{4,30})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$/"; // . indicates all the characters. * indicates at least 0 times pattern should be repeated
+                            if (!preg_match($pattern_pass, $password)) {
+                                $errpass = "Must be at least 4 characters long, 1 upper case, 1 lower case and 1 number";
+                            }
+                        } else {
+                            $errpass ="Password doesn't matched";
+                        }
+                        if(!isset($errpass)){
+                        $query = "SELECT * FROM user_details WHERE email = '$email' AND validation_key = '$validation_key' AND is_active=1";
+                        $query_run = mysqli_query($connection, $query);
+
+                        if(!$query_run){
+                          die("Unable to connect to database".mysqli_error($connection));
+                        }
+
+                        if(mysqli_num_rows($query_run)==1){
+
+                            $password_hash = password_hash($password, PASSWORD_BCRYPT, ['cost'=>10]);
+
+                            //update password
+                            $query1 = "UPDATE user_details SET password = '$password_hash' WHERE validation_key = '$validation_key' AND email='$email' AND is_active=1";
+                            $query_run1 = mysqli_query($connection, $query1);
+                            if(!$query_run1){
+                              die("Connection to database failed! ".mysqli_error($connection));
+                            }else {
+                              $query2 = "UPDATE user_details SET validation_key=0 WHERE validation_key='$validation_key' AND email = '$email' AND is_active=1";
+                              $query_run2 = mysqli_query($connection, $query2);
+                              if(!$query_run2){
+                                die("Unable to connect to database".mysqli_error());
+                              }
+                              echo "<div class='notification'>Password has successfully updated.</div>";
+                              header("Refresh:3;url=login.php");
+                            }
+                          }else echo "<div class='notification'>Invalid link</div>";
+                        }
+                      }
+
+}
+                  }else {
+                    echo "<div class='notification'>Something went wrong:(</div>";
+                  }
+              ?>
+            <!-- <div class='notification'>Password updated successfully. <a href='login.php'>login now</a></div> -->
             <form action="" method="POST">
                 <div class="input-box">
-                    <input type="password" class="input-control" placeholder="New password" name="new-password" required>
+                    <input type="password" class="input-control" placeholder="New password" name="new-password" <?php echo isset($check)? "disabled": ""?> required>
+                    <?php echo isset($errpass)?"<span class='error'>$errpass</span>" : ""; ?>
                 </div>
                 <div class="input-box">
-                    <input type="password" class="input-control" placeholder="Confirm new password" name="confirm-new-password" required>
+                    <input type="password" class="input-control" placeholder="Confirm new password" name="confirm-new-password" <?php echo isset($check)? "disabled": ""?> required>
                 </div>
                 <div class="input-box">
                     <input type="submit" class="input-submit" value="SUBMIT" name="submit" >
